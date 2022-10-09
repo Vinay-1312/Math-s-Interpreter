737ceb471e63378124761a94cd70d03992b3550f
Vinay-1312 <66013055+Vinay-1312@users.noreply.github.com>
2022-10-09 20:27:41 +0530

    Add files via upload
    Initial Commit

---
+++ Maths Interpreter/Maths Interpreter/MainProg.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Maths_Interpreter
+{
+    public class MainProg
+    {
+        public static void Main(string[] args)
+        {
+            while(true)
+            {
+                Console.Write("Expr > ");
+                string input = Console.ReadLine();
+                Lexer lexer = new Lexer(input);
+                var tokens = lexer.GenerateTokens();
+                Console.WriteLine(string.Join(",",tokens));
+            }
+        }
+    }
+}
---
+++ Maths Interpreter/Maths Interpreter/Tokens.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Maths_Interpreter
+{
+    //Add Tokens here
+    public class Tokens
+    {
+        public int plus = 1;
+        public int subtract = 2;
+        public int multiplication = 3;
+        public int division = 4;
+        public int percentage = 5;
+        public int leftParenthesis = 6;
+        public int rightParenthesis = 7;
+        public int number = 8;
+
+    }
+
+}
